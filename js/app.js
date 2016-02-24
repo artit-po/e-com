8aab527d8d98bab5baa5317188864bdb93f79196
artit-po <po_mal@hotmail.co.th>
2016-02-24 23:17:27 +0700

    Create app.js
    router in angular

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//router angular single page 
+
+angular.module('application', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/home.html'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: 'templates/register.html'
+            })
+            .state('slide', {
+                url: '/slide',
+                templateUrl: 'templates/slide.html'
+            })
+        .state('contact',{
+            url:'/contact',
+            templateUrl: 'templates/contact.html'
+        });
+    });
+
